38438d7b19e612aaef0deca259fb2093b9d36391
Jonathan Johnson <jon@nilobject.com>
2011-03-22 16:02:33 -0500

    Finished initial commit.

---
+++ RedminerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RedminerAppDelegate.h
+//  Redminer
+//
+//  Created by Jonathan Johnson on 3/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RedminerAppDelegate : NSObject <NSApplicationDelegate> {
+    NSWindow *window;
+}
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ RedminerAppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  RedminerAppDelegate.m
+//  Redminer
+//
+//  Created by Jonathan Johnson on 3/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "RedminerAppDelegate.h"
+
+@implementation RedminerAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+@end
